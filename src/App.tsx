1eaf493534d1b837b153e91dd5b740b9d0d8b423
viniciusruiiz <vinic@MACHINA>
2024-10-11 20:15:32 -0300

    initial project strcture

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center text-7xl'>
+      NVR Sup
+    </div>
+  )
+}
+
+export default App
